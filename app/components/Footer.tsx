4cb484b884efd706d329db8b8ba9eb8283c78480
eldervsoriano <eldervsoriano@gmail.com>
2025-11-25 00:35:00 +0800

    Revert "Start"
    This reverts commit c44b134f4ba67bb5c6471b4b85d63e1d0c8e1dd2.

--- app/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="bg-gray-900 text-center py-6 text-gray-400 text-sm">
-            © 2025 Elder Soriano. All rights reserved.
-        </footer>
-    );
-}
